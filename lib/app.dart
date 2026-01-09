f23fa0e7f2c40cebb2ba9598d0efc2ae2c8b27f4
sumantaghorai44-svg <sumantaghorai44@gmail.com>
2026-01-09 21:00:26 +0530

    Fix app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
-class RoomExpenseApp extends StatelessWidget {
-  const RoomExpenseApp({super.key});
+class RoomExpensesApp extends StatelessWidget {
+  const RoomExpensesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Room Expenses',
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      home: const Scaffold(
-        body: Center(child: Text('Room Expenses App')),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
     );
   }
 }
