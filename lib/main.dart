f8afeff6feb28c020833551afa08a130ecd5990c
sumantaghorai44-svg <sumantaghorai44@gmail.com>
2026-01-09 20:49:42 +0530

    Fix RoomExpensesApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'home.dart';
 
-void main() {
-  runApp(const RoomExpensesApp());
+class RoomExpensesApp extends StatelessWidget {
+  const RoomExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Room Expenses',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
