7a92023f5ceb3f70bb0f5c0199aff9014e5119f3
sumantaghorai44-svg <sumantaghorai44@gmail.com>
2026-01-09 20:58:57 +0530

    Add home screen

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Room Expenses')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(onPressed: () {}, child: const Text('Add Expense')),
+          ElevatedButton(onPressed: () {}, child: const Text('View History')),
+          ElevatedButton(onPressed: () {}, child: const Text('Dashboard')),
+        ],
+      ),
+    );
+  }
+}
