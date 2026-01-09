caf4545f9647988213ff739bca45607b32b0c5e9
sumantaghorai44-svg <sumantaghorai44@gmail.com>
2026-01-09 20:42:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Room Expenses',
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Room Expenses')),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Add Expense'),
-            ),
-            const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('View History'),
-            ),
-            const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Dashboard'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  runApp(const RoomExpensesApp());
 }
