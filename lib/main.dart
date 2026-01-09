bbe8a29d5f2ff16d802db5d04e244bf7e4eff718
sumantaghorai44-svg <sumantaghorai44@gmail.com>
2026-01-09 21:01:47 +0530

    Fix main entry

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'app.dart';
 
-class RoomExpensesApp extends StatelessWidget {
-  const RoomExpensesApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Room Expenses',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: const HomeScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
+void main() {
+  runApp(const RoomExpensesApp());
 }
